0c7319187cc14fa6cb01b9b14042b8249b766977
Mike Kistler <mikekistler@microsoft.com>
2025-04-08 11:07:19 -0700

    Repro of FromServices parameter getting generated validation

--- validation-from-services/Program.cs
+++ validation-from-services/Program.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddValidation();
 
 var app = builder.Build();
 
@@ -19,9 +25,9 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", ([FromServices] IOptions<WeatherServiceOptions> options, [Range(1,5)] int count) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast =  Enumerable.Range(1, count).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -38,4 +44,10 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class WeatherServiceOptions
+{
+    [Required]
+    public string? Foo { get; set; }
 }
